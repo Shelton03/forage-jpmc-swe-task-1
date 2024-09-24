2a49e4992d13c7d961051e53ab54c9f82f02fdc4
Shelton Mutambirwa <sheltonmutambirwa@gmail.com>
2024-09-24 22:06:05 +0200

    fixed the problem

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
+import random
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,18 +8,37 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    choice = random.choice(quotes)
+    self.assertEqual(getDataPoint(choice),(choice['stock'],choice['top_bid']['price'],
+                                           choice['top_ask']['price'],(choice['top_bid']['price']+ choice['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+  
+    
+    choice = random.choice(quotes)
+    self.assertEqual(getDataPoint(choice),(choice['stock'],choice['top_bid']['price'],
+                                           choice['top_ask']['price'],(choice['top_bid']['price']+ choice['top_ask']['price'])/2))
 
 
-  """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio_WithZero(self):
+    prices = [{'A':98.955,'B':0},{'A':0,'B':0}]
+
+    choice = random.choice(prices)
+    self.assertEqual(getRatio(choice['A'],choice['B']),None)
+  
+  def test_getRatio_WithZero(self):
+    prices = [{'A':98.955,'B':95.567},{'A':123.01,'B':143.564}]
+
+    choice = random.choice(prices)
+    self.assertEqual(getRatio(choice['A'],choice['B']),(choice['A']/choice['B']))
+
+  
+     
 
 
 if __name__ == '__main__':
